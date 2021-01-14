39dde34262be4285fd2019627be542416ef4581a
Omar Chammaa <chammaa.omar@gmail.com>
2021-01-14 00:17:07 -0500

    pick players fragment and data binding

--- app/src/main/java/com/example/pennydrop/fragments/PickPlayersFragment.kt
+++ app/src/main/java/com/example/pennydrop/fragments/PickPlayersFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.pennydrop.R
+import com.example.pennydrop.databinding.FragmentPickPlayersBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,7 +36,8 @@ class PickPlayersFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pick_players, container, false)
+        val binding = FragmentPickPlayersBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
